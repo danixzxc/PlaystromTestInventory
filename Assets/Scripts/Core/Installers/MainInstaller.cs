1c6e7a7f44d6215892408f775f7aff29dd9148bd
danixzxc <danilagrisel@gmail.com>
2026-07-24 19:29:38 +0300

    Added Zenject Installer scripts

---
+++ Assets/Scripts/Core/Installers/MainInstaller.cs
@@ -0,0 +1,129 @@
+using Zenject;
+using UnityEngine;
+using Core.EventBus;
+using Core.Pool;
+using Data.ScriptableObjects;
+using Modules.Inventory.Model;
+using Modules.Inventory.View;
+using Modules.Inventory.Presenter;
+using Modules.Chest.Model;
+using Modules.Chest.View;
+using Modules.Chest.Presenter;
+using Modules.Drop.View;
+using Modules.Drop.Factory;
+using Modules.Bonus.Model;
+using Modules.Bonus.Presenter;
+using Services;
+
+namespace Core.Installers
+{
+    public class MainInstaller : MonoInstaller
+    {
+        [Header("Configs")]
+        [SerializeField] private ChestConfig _chestConfig;
+        [SerializeField] private InventoryConfig _inventoryConfig;
+        [SerializeField] private BonusConfig _bonusConfig;
+
+        [Header("UI Targets")]
+        [SerializeField] private CoinIndicatorView _coinIndicatorView;
+        [SerializeField] private CrystalIndicatorView _crystalIndicatorView;
+        [SerializeField] private HealthBarView _healthBarView;
+
+        [Header("Chest")]
+        [SerializeField] private ChestView _chestView;
+
+        [Header("Drop Prefabs")]
+        [SerializeField] private BaseDropItemView _coinDropPrefab;
+        [SerializeField] private BaseDropItemView _crystalDropPrefab;
+        [SerializeField] private BaseDropItemView _healthDropPrefab;
+
+        [Header("Pool Settings")]
+        [SerializeField] private int _poolInitialSize = 10;
+        [SerializeField] private Transform _poolParent;
+
+        public override void InstallBindings()
+        {
+            BindConfigs();
+            BindModels();
+            BindViews();
+            BindPools();
+            BindFactories();
+            BindServices();
+            BindPresenters();
+        }
+
+        private void BindConfigs()
+        {
+            Container.Bind<ChestConfig>().FromInstance(_chestConfig).AsSingle();
+            Container.Bind<InventoryConfig>().FromInstance(_inventoryConfig).AsSingle();
+            Container.Bind<BonusConfig>().FromInstance(_bonusConfig).AsSingle();
+        }
+
+        private void BindModels()
+        {
+            Container.Bind<InventoryModel>().AsSingle();
+            Container.Bind<ChestModel>().AsSingle();
+            Container.Bind<BonusModel>().AsSingle();
+        }
+
+        private void BindViews()
+        {
+            Container.Bind<CoinIndicatorView>().FromInstance(_coinIndicatorView).AsSingle();
+            Container.Bind<CrystalIndicatorView>().FromInstance(_crystalIndicatorView).AsSingle();
+            Container.Bind<HealthBarView>().FromInstance(_healthBarView).AsSingle();
+            Container.Bind<ChestView>().FromInstance(_chestView).AsSingle();
+        }
+
+        private void BindPools()
+        {
+            Container.Bind<ObjectPool>()
+                .WithId("CoinPool")
+                .FromInstance(new ObjectPool(_coinDropPrefab, _poolInitialSize, _poolParent))
+                .AsCached();
+
+            Container.Bind<ObjectPool>()
+                .WithId("CrystalPool")
+                .FromInstance(new ObjectPool(_crystalDropPrefab, _poolInitialSize, _poolParent))
+                .AsCached();
+
+            Container.Bind<ObjectPool>()
+                .WithId("HealthPool")
+                .FromInstance(new ObjectPool(_healthDropPrefab, _poolInitialSize, _poolParent))
+                .AsCached();
+        }
+
+        private void BindFactories()
+        {
+            Container.Bind<DropItemFactory>().AsSingle();
+        }
+
+        private void BindServices()
+        {
+            Container.Bind<AnimationService>().AsSingle();
+
+            Container.Bind<RectTransform>()
+                .WithId("CoinTarget")
+                .FromInstance(_coinIndicatorView.RectTransform);
+
+            Container.Bind<RectTransform>()
+                .WithId("CrystalTarget")
+                .FromInstance(_crystalIndicatorView.RectTransform);
+
+            Container.Bind<RectTransform>()
+                .WithId("HealthTarget")
+                .FromInstance(_healthBarView.RectTransform);
+
+            Container.Bind<CollectionService>().AsSingle();
+            Container.Bind<BonusService>().AsSingle();
+        }
+
+        private void BindPresenters()
+        {
+            Container.Bind<CoinPresenter>().AsSingle().NonLazy();
+            Container.Bind<CrystalPresenter>().AsSingle().NonLazy();
+            Container.Bind<HealthPresenter>().AsSingle().NonLazy();
+            Container.Bind<ChestPresenter>().AsSingle().NonLazy();
+            Container.Bind<BonusPresenter>().AsSingle().NonLazy();
+        }
+    }
+}
