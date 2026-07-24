8d91a5cb5cd5f0bf09c7fb560deb874f07686190
danixzxc <danilagrisel@gmail.com>
2026-07-24 23:13:23 +0300

    Connect presenter with view in DropItemFactory

--- Assets/Scripts/Modules/Drop/Factory/DropItemFactory.cs
+++ Assets/Scripts/Modules/Drop/Factory/DropItemFactory.cs
@@ -3,6 +3,9 @@ using Data.ScriptableObjects;
 using Modules.Drop.View;
 using UnityEngine;
 using Zenject;
+using Modules.Drop.Presenter;
+using Services;
+using System.Collections.Generic;
 
 namespace Modules.Drop.Factory
 {
@@ -11,15 +14,19 @@ namespace Modules.Drop.Factory
         private readonly ObjectPool _coinPool;
         private readonly ObjectPool _crystalPool;
         private readonly ObjectPool _healthPotionPool;
+        private readonly CollectionService _collectionService;
+        private readonly List<DropItemPresenter> _activePresenters = new List<DropItemPresenter>();
 
         public DropItemFactory(
-        [Inject(Id = "CoinPool")] ObjectPool coinPool,
-        [Inject(Id = "CrystalPool")] ObjectPool crystalPool,
-        [Inject(Id = "HealthPool")] ObjectPool healthPotionPool)
+            [Inject(Id = "CoinPool")] ObjectPool coinPool,
+            [Inject(Id = "CrystalPool")] ObjectPool crystalPool,
+            [Inject(Id = "HealthPool")] ObjectPool healthPotionPool,
+            CollectionService collectionService)
         {
             _coinPool = coinPool;
             _crystalPool = crystalPool;
             _healthPotionPool = healthPotionPool;
+            _collectionService = collectionService;
         }
 
         public BaseDropItemView CreateDrop(DropItemConfig config, Vector3 position)
@@ -27,6 +34,10 @@ namespace Modules.Drop.Factory
             BaseDropItemView dropView = GetDropFromPool(config.ItemType);
             dropView.transform.position = position;
             dropView.SetSprite(config.Icon);
+
+            DropItemPresenter presenter = new DropItemPresenter(dropView, config, _collectionService);
+            _activePresenters.Add(presenter);
+
             return dropView;
         }
 
