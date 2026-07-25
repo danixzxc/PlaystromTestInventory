6c9ccfb7b7a5bc272ec5f2e1a7eb47569c1f94af
danixzxc <danilagrisel@gmail.com>
2026-07-26 00:30:14 +0300

    Added DoScale animation to item icons on collect

--- Assets/Scripts/Modules/Inventory/View/CoinIndicatorView.cs
+++ Assets/Scripts/Modules/Inventory/View/CoinIndicatorView.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,14 +8,37 @@ namespace Modules.Inventory.View
     public class CoinIndicatorView : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _amountText;
+        [SerializeField] private Image _iconImage;
+        [SerializeField] private float _tweenDuration = 0.3f;
+        [SerializeField] private float _scaleMultiplier = 1.3f;
+
+        private Tweener _currentTweener;
 
         public void Initialize(int initialAmount)
         {
             _amountText.text = initialAmount.ToString();
+            _iconImage.transform.localScale = Vector3.one;
         }
+
         public void UpdateAmount(int amount)
         {
+            if (_amountText.text == amount.ToString())
+            {
+                return;
+            }
             _amountText.text = amount.ToString();
+
+            _currentTweener?.Kill();
+
+            _iconImage.transform.localScale = Vector3.one;
+
+            _currentTweener = _iconImage.transform
+                .DOScale(Vector3.one * _scaleMultiplier, _tweenDuration * 0.5f)
+                .OnComplete(() =>
+                {
+                    _currentTweener = _iconImage.transform
+                        .DOScale(Vector3.one, _tweenDuration * 0.5f);
+                });
         }
     }
 }
