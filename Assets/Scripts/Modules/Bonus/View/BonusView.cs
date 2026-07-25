73202837bf93c3cb2d79974d5e5e728f36bb05b8
danixzxc <danilagrisel@gmail.com>
2026-07-26 00:33:03 +0300

    Added DoScale animation to bonus indicator

--- Assets/Scripts/Modules/Bonus/View/BonusView.cs
+++ Assets/Scripts/Modules/Bonus/View/BonusView.cs
@@ -1,4 +1,5 @@
 using Data.ScriptableObjects;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,13 +13,19 @@ namespace Modules.Bonus.View
         [SerializeField] private Color _coinColor = Color.yellow;
         [SerializeField] private Color _healthColor = Color.green;
         [SerializeField] private Color _crystalColor = Color.cyan;
+        [SerializeField] private float _scaleInDuration = 0.3f;
+        [SerializeField] private float _scaleOutDuration = 0.3f;
+        [SerializeField] private float _scaleMultiplier = 1.2f;
 
         public RectTransform RectTransform { get; private set; }
+
+        private Tweener _currentTweener;
 
         private void Awake()
         {
             RectTransform = GetComponent<RectTransform>();
             _bonusIndicator.SetActive(false);
+            _bonusIndicator.transform.localScale = Vector3.zero;
         }
 
         public void ShowBonus(float duration, DropItemType bonusType)
@@ -26,6 +33,13 @@ namespace Modules.Bonus.View
             _bonusIndicator.SetActive(true);
             _bonusTimerSlider.value = 1f;
             _sliderFillImage.color = GetColorByType(bonusType);
+
+            _currentTweener?.Kill();
+            _bonusIndicator.transform.localScale = Vector3.zero;
+
+            _currentTweener = _bonusIndicator.transform
+                .DOScale(Vector3.one * _scaleMultiplier, _scaleInDuration)
+                .SetEase(Ease.OutBack);
         }
 
         public void UpdateTimer(float progress)
@@ -35,7 +49,15 @@ namespace Modules.Bonus.View
 
         public void HideBonus()
         {
-            _bonusIndicator.SetActive(false);
+            _currentTweener?.Kill();
+
+            _currentTweener = _bonusIndicator.transform
+                .DOScale(Vector3.zero, _scaleOutDuration)
+                .SetEase(Ease.InBack)
+                .OnComplete(() =>
+                {
+                    _bonusIndicator.SetActive(false);
+                });
         }
 
         private Color GetColorByType(DropItemType type)
